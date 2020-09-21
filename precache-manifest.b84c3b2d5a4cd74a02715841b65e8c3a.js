c8568c8e22031ca369b11df37536863d51b0fbe3
sri <srimanigoud@gmail.com>
2020-09-21 11:46:35 +0530

    Updates

---
+++ precache-manifest.b84c3b2d5a4cd74a02715841b65e8c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26cbad9a7a713e8b7cb883cee3db5d4",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "b57307e413bf27c71709",
+    "url": "/Monsters/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57307e413bf27c71709",
+    "url": "/Monsters/static/js/main.9d725f1d.chunk.js"
+  },
+  {
+    "revision": "99d8a27463727051162c",
+    "url": "/Monsters/static/js/runtime-main.3abc2b3c.js"
+  }
+]);
